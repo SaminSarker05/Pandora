dc5eee1153e1bf4929d9e0b24008c744bf5494f8
Samin Sarker <saminsarker05@gmail.com>
2024-09-25 01:06:42 -0400

    error with subtract function in adder

--- main.cpp
+++ main.cpp
@@ -5,8 +5,10 @@
 int main() {
   auto res = EightBitAdder::add(3, 4, 0);
   std::cout << res.first << std::endl;  // expecting 7
+  Comparator check(3, 4);
 
-
+  std::cout << check.less_than() << " " << check.greater_than()
+  << " " << check.equal_to() << std::endl;
 
   return 0;
 }
