a186d22fdb5d305523a62795c70e1aa55cb99ee4
Samin Sarker <saminsarker05@gmail.com>
2024-09-28 01:58:52 -0400

    deleted files

--- main.cpp
+++ main.cpp
@@ -1,14 +1,6 @@
 #include <iostream>
-#include "Comparator.h"
-#include "EightBitAdder.h"
 
-int main() {
-  auto res = EightBitAdder::add(3, 4, 0);
-  std::cout << res.first << std::endl;  // expecting 7
-  Comparator check(3, 4);
-
-  std::cout << check.less_than() << " " << check.greater_than()
-  << " " << check.equal_to() << std::endl;
-
+int main() 
+{
   return 0;
 }
